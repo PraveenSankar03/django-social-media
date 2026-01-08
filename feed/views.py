c82844096839a0341051b3f2597b43dd4151c0aa
Praveen <praveensankar3333@gmail.com>
2026-01-08 20:22:39 +0530

    added new post

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 class Homepage(ListView):
@@ -13,3 +14,9 @@ class PostDetailView(DetailView):
     template_name = 'feed/detail.html'
     model = Post
     context_object_name = 'post'
+
+class CreateNewPost(CreateView):
+    http_method_names = ['get']
+    template_name = 'feed/create.html'
+    model = Post
+    fields = ['text']
