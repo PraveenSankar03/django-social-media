58306f968aaa770f11d934a857ffb624c240de73
Praveen <praveensankar3333@gmail.com>
2026-01-10 12:47:39 +0530

    ajaxing complete

--- til/settings.py
+++ til/settings.py
@@ -158,3 +158,10 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS= [
+    os.path.join(PROJECT_DIR, 'frontend/')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
